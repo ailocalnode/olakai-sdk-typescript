3c0714fb0bdb08dc5211f0a375f2d2c6ba9ad4fd
SaschaR0de <sascha@olakai.ai>
2025-07-22 16:06:30 -0700

    delete /dist (will be in the .gitignore now)

--- dist/src/middleware/caching.d.ts
+++
@@ -1,7 +0,0 @@
-import { Middleware } from "./index";
-export declare function createCachingMiddleware<TArgs extends any[], TResult>(options: {
-    ttlMs?: number;
-    keyGenerator: (args: TArgs) => string;
-    maxSize?: number;
-}): Middleware<TArgs, TResult>;
-//# sourceMappingURL=caching.d.ts.map
--- dist/src/middleware/logging.d.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware } from "./index";
-export declare function createLoggingMiddleware<TArgs extends any[], TResult>(options: {
-    level?: "debug" | "info" | "warn" | "error";
-    includeArgs?: boolean;
-    includeResult?: boolean;
-    logger?: Console;
-}): Middleware<TArgs, TResult>;
-//# sourceMappingURL=logging.d.ts.map
